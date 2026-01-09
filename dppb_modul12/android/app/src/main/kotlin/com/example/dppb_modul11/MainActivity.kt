174aad1bbfc314b957dfc8ef31f3be0cfecd238b
“belvaedria” <belvaedriaz@gmail.com>
2026-01-09 07:43:21 +0700

    Clear repo contents

--- dppb_modul12/android/app/src/main/kotlin/com/example/dppb_modul11/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dppb_modul11
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- ngangkot/android/app/src/main/kotlin/com/example/aplikasialerts/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aplikasialerts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
